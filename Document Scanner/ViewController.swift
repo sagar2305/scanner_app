bd1e169c89e2f6797008a5ffc43a6594ec54f89c
sandesh-naik <sandesh@indianrails.in>
2021-03-04 10:03:44 +0530

    Initial Commit

---
+++ Document Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Document Scanner
+//
+//  Created by Sandesh on 04/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
